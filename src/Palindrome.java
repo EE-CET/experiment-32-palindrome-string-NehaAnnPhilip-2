20e5fc182868fc02e199a46f16459045c0acbb16
NehaAnnPhilip <nehaphilip0205@gmail.com>
2026-04-10 01:08:42 +0530

    Update Palindrome.java

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,8 +1,37 @@
+import java.util.Scanner;
+
 public class Palindrome {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         // TODO: Read the string
+        if (!sc.hasNextLine()) return;
+        String input = sc.nextLine();
+
         // TODO: Convert to lower case (to handle 'Malayalam' correctly)
+        String s = input.toLowerCase();
+
         // TODO: Check if the string is the same reversed
+        boolean isPalindrome = true;
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
         // TODO: Print "1" if palindrome, "0" if not
-    
+        if (isPalindrome) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+        
+        sc.close();
+    }
 }
